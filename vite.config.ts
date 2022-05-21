735525247a42de8302d08bdb9fcc3c6aca12e39e
Jacobtread <jacobtread@gmail.com>
2022-05-21 16:13:59 +1200

    Set dist to output to main project

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+const kmePath = path.join('..', 'src', 'main', 'resources', 'panel')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/panel/',
+  build: {
+    outDir: kmePath
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
