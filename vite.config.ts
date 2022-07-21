0e37227abcd81f263bb1631a9cf31ab7d6f97779
Jacobtread <jacobtread@gmail.com>
2022-07-21 22:05:19 +1200

    Updated config for new path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-const kmePath = path.join('..', 'src', 'main', 'resources', 'panel')
+const kmePath = path.join('..', 'app', 'src', 'main', 'resources', 'panel')
 
 // https://vitejs.dev/config/
 export default defineConfig({
